943012d8092ed2a8be297e9dc04eeed38964ca8e
David Rogers <rogersdpdr@gmail.com>
2023-06-21 16:06:56 +0200

    wip

--- gen.rb
+++ gen.rb
@@ -21,6 +21,7 @@ end
 Dotenv.load
 
 GithubAPIError = StandardError.new
+GitlabAPIError = StandardError.new
 
 def github_response(content) # rubocop:disable Metrics/MethodLength, Metrics/AbcSize
   uri = URI.parse('https://api.github.com/markdown')
@@ -40,7 +41,21 @@ def github_response(content) # rubocop:disable Metrics/MethodLength, Metrics/Abc
   response.body
 end
 
-def gitlab_response
+def gitlab_response(content) # rubocop:disable Metrics/MethodLength, Metrics/AbcSize
+  uri = URI.parse('https://gitlab.com/api/v4/markdown')
+  params = { 'text' => content.to_s, 'gfm' => 'true' }
+  request = Net::HTTP::Post.new(uri)
+  request['Content-Type'] = 'application/json'
+  request['PRIVATE-TOKEN'] = ENV['GITLAB_API_KEY'].to_s
+
+  request.body = params.to_json
+
+  response = Net::HTTP.start(uri.host, uri.port, use_ssl: true) do |http|
+    http.request(request)
+  end
+  raise GitlabAPIError unless response.code == '201'
+
+  JSON.parse(response.body)
 end
 
 # Parses the raw file and returns seperate yaml metadata and markdown content
@@ -64,16 +79,45 @@ end
 def read_yaml(yaml)
   return if yaml.nil?
 
-  YAML::load(yaml).transform_keys(&:to_sym)
+  YAML.load(yaml).transform_keys(&:to_sym)
+end
+
+def beginning_of_file(title)
+  "---
+import Blog from '../../layouts/Blog.astro';
+import '../../markdown.css';
+---
+
+  <Blog title='#{title}'>
+  "
+end
+
+def end_of_file
+  "\n</Blog>"
+end
+
+def output(metadata, content)
+  title = metadata[:title]
+  beginning_of_file(title) + content + end_of_file
+end
+
+Dir.glob("#{SOURCE_PATH}/**/*.md").each do |file|
+pp file
 end
 
 Dir.glob("#{SOURCE_PATH}/**/*.md").each do |file|
   content = File.open(file).read
   parsed_file = Content.new(content)
   metadata = read_yaml(parsed_file.yaml)
-  html = github_response(parsed_file.markdown)
-  pp html
+  next if metadata.nil?
+  ghhtml = github_response(parsed_file.markdown)
+  glhtml = gitlab_response(parsed_file.markdown)
   file = file.gsub("#{SOURCE_PATH}/", '')
+  gl_file = file.gsub('.md', '_gitlab.astro')
+  gh_file = file.gsub('.md', '_github.astro')
+  pp gl_file
+  pp gh_file
   pp file
-  File.write(OUTPUT_PATH + '/' + file, html)
+  File.write("#{OUTPUT_PATH}/#{gh_file}", output(metadata, ghhtml))
+  File.write("#{OUTPUT_PATH}/#{gl_file}", output(metadata, glhtml['html']))
 end
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,8 +2,12 @@
 module.exports = {
   darkMode: "class",
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  fontFamily: {
+    sans: ['Graphik', 'sans-serif'],
+    serif: ['Merriweather', 'serif'],
+  },
 	theme: {
 		extend: {},
 	},
-	plugins: [],
+	plugins: [{'postcss-import': {}}]
 }

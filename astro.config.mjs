cd67fe5a5a732f87f13e542728998774870dedc3
David Rogers <rogersdpdr@gmail.com>
2023-07-22 15:54:56 +0200

    add view

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from 'astro/config';
 import svelte from '@astrojs/svelte';
 import tailwind from '@astrojs/tailwind';
+import vue from '@astrojs/vue';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [svelte(), tailwind({
+  integrations: [svelte(), vue(),tailwind({
     // Example: Disable injecting a basic `base.css` import on every page.
     // Useful if you need to define and/or import your own custom `base.css`.
     config: { applyBaseStyles: false },
---
+++ src/components/til/search.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <input
+      type="text"
+      v-model="searchText"
+      @input="handleSearch"
+      placeholder="Search..."
+    />
+  </div>
+</template>
+
+<script>
+import { defineComponent, ref } from 'vue';
+
+export default defineComponent({
+  name: 'Search',
+  setup() {
+    const searchText = ref('');
+
+    const handleSearch = () => {
+      // Emit the 'search' event with the current search text
+      if (searchText.value.trim()) {
+        const trimmedText = searchText.value.trim();
+        // Emit the 'search' event with the trimmed search text
+        emit('search', trimmedText);
+      }
+    };
+
+    return { searchText, handleSearch };
+  },
+});
+</script>

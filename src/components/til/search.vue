0958f58c8a789eb4b446b080c5d54c9ab1818b72
David Rogers <rogersdpdr@gmail.com>
2023-07-28 17:35:29 +0200

    add search to vue til

--- src/components/til/search.vue
+++ src/components/til/search.vue
@@ -5,7 +5,7 @@
       <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
         <svg aria-hidden="true" class="w-5 h-5 text-gray-500 dark:text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
       </div>
-      <input type="search" v-model="searchText" name="search" id="blogSearch" class="block p-4 pl-10 text-sm text-gray-900 border-b border-gray-300 bg-transparent dark:border-gray-600 dark:placeholder-gray-400 dark:text-white border-b focus:outline-none dark:focus:border-sky-400 focus:border-sky-500 w-full" placeholder="Search for a Blog...">
+      <input type="search" v-model="searchText" name="search" id="blogSearch" class="block p-4 pl-10 text-sm text-gray-900 border-b border-gray-300 bg-transparent dark:border-gray-600 dark:placeholder-gray-400 dark:text-white border-b focus:outline-none dark:focus:border-sky-400 focus:border-sky-500 w-full" placeholder="Search for a Lesson...">
       <button type="submit" class="text-white absolute right-2.5 bottom-2.5 bg-sky-400 hover:bg-sky-700 font-medium rounded-lg text-sm px-4 py-2 dark:bg-sky-500 dark:hover:bg-sky-700 " @click="handleSearch">Search</button>
     </div>
   </form>
@@ -16,14 +16,23 @@
 import { defineComponent, ref } from 'vue';
 import json from '../../../blogs.json';
 import List from './list.vue';
+import Fuse from 'fuse.js';
 
 export default defineComponent({
   name: 'Search',
   components: { List },
   methods: {
     handleSearch() {
-      console.log("Form submitted", this.searchText);
-      // Your form handling logic here, using this.searchText.value
+      if (this.searchText === "") {
+        this.til = json.til
+        return
+      }
+      const options = {
+        keys: ['title', 'excerpt', 'topics'],
+      }
+      const fuse = new Fuse(json.til, options);
+      const res = fuse.search(this.searchText).map(obj => obj.item);
+      this.til = res
     }
   },
   data() {
